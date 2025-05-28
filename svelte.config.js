f06dcccddc5f942352597f83682ae0154c89adb3
Chris <christian.bang@gmail.com>
2025-05-28 16:04:41 +0200

    fix deployment thing

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'development' ? '/banana' : '/banana',
+			base: process.env.NODE_ENV === 'development' ? '/banana' : '',
 		},
 		csrf: { checkOrigin: true }
 
